8d42792731f5b328e230e6f98e8bb07e588fa2be
Arianna Grassi <ariannagrassi12@gmail.com>
2024-04-08 14:32:55 +0200

    login

---
+++ src/main/java/com/example/eros2/Login.java
@@ -0,0 +1,28 @@
+package com.example.eros2;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@WebServlet("/login")
+public class Login extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	private static final Logger log = LogManager.getLogger(Login.class);
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String nickname = request.getParameter("nickname");
+		String password = request.getParameter("password");
+		log.trace("{} {}", nickname, password); // non si fa!
+		request.getRequestDispatcher("login.jsp").forward(request, response);
+
+	}
+
+}

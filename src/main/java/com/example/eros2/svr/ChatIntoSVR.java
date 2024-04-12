a80b1459050633ece9fa9b56f3e6013f76d44471
GenFox <gennaroloffredo02@gmail.com>
2024-04-12 12:30:04 +0200

    New file for chat

--- src/main/java/com/example/eros2/svr/ChatIntoSVR.java
+++ src/main/java/com/example/eros2/svr/ChatIntoSVR.java
@@ -1,1 +1,28 @@
 package com.example.eros2.svr;
+
+
+//// Getting the names of users
+//String senderUserName = userDao.getUserNameById(senderUserID);
+//String receiverUserName = userDao.getUserNameById(receiverUserID);
+// for select
+//// instance chat
+//Chat chat = new Chat();
+//chat.setMessage(message);
+//chat.setSenderUserID(senderUserID);
+//chat.setReceiverUserID(receiverUserID);
+//chat.setTimestamp(LocalDateTime.now());
+
+//// Add message using DAO 
+//try {
+////  chatDao.insertMessage(chat);
+////  //Send a JSON response to confirm success
+////  response.setContentType("application/json");
+////  response.setCharacterEncoding("UTF-8");
+////  response.getWriter().write("{\"success\": true}");
+//} catch (SQLException e) {
+//  e.printStackTrace();
+//  // Send a JSON response to report the error
+//  response.setContentType("application/json");
+//  response.setCharacterEncoding("UTF-8");
+//  response.getWriter().write("{\"success\": false, \"error\": \"" + e.getMessage() + "\"}");
+//}
